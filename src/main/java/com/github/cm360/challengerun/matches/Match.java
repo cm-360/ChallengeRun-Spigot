754d02145fc451a13d1115211174c4fcb2385ade
cm360 <cm360@tron>
2023-03-15 20:01:11 -0400

    Idk man, more match implementation things

--- src/main/java/com/github/cm360/challengerun/matches/Match.java
+++ src/main/java/com/github/cm360/challengerun/matches/Match.java
@@ -8,15 +8,20 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import com.github.cm360.challengerun.challenges.Challenge;
+import com.github.cm360.challengerun.events.ChallengeCompletedEvent;
 import com.github.cm360.challengerun.events.MatchCompletedEvent;
 import com.github.cm360.challengerun.main.ChallengeRunPlugin;
 
 /**
  * The Class Match.
  */
-public class Match {
+public class Match implements Listener {
 
 	/** The players in this match. */
 	private Map<UUID, Integer> playersAndScores = new HashMap<UUID, Integer>();
@@ -24,17 +29,25 @@ public class Match {
 	/** The players that have voted to skip the current challenge. **/
 	private Set<UUID> votes = new HashSet<UUID>();
 	
+	private boolean votingAllowed = false;
+	
 	/** The current challenge. */
 	private Challenge currentChallenge;
 	
 	/** The number of minutes to put on the prep timer. */
-	private int prepTimerMins;
+	private int prepTimerMins = 5;
+	
+	/** The number of minutes to put on the voting timer. */
+	private int votingTimerMins = 2;
 	
 	/** The number of minutes to put on the challenge timer. */
-	private int challengeTimerMins;
+	private int challengeTimerMins = 10;
 	
-	/** The task ID of the currently running timer. */
-	private int timerTaskId;
+	/** The task ID of the currently running main timer. */
+	private int mainTimerTaskId;
+	
+	/** The task ID of the voting timer. */
+	private int votingTimerTaskId;
 	
 	/**
 	 * Adds a player to this match.
@@ -63,23 +76,25 @@ public class Match {
 	}
 	
 	/**
-	 * Starts the match.
+	 * Starts the match and registers its event handlers.
 	 */
 	public void start() {
 		announce("Welcome to Challenge Run! Your objective is to complete as many challenges as you can.");
 		announce(String.format("You will have %d minutes to prepare before being given your first task, good luck!", prepTimerMins));
-		startPrepTimer();
+		startPrepPeriod();
+		Bukkit.getPluginManager().registerEvents(this, ChallengeRunPlugin.instance);
 	}
 	
 	/**
-	 * Ends the match and announces the scores.
+	 * Ends the match and announces the scores. Calling will also unregister this
+	 * match's event handlers.
 	 */
 	public void end() {
-		cancelTimer();
 		endChallenge();
 		announce("The game is over! Lets see how everyone did!");
 		annouceScores();
 		Bukkit.getPluginManager().callEvent(new MatchCompletedEvent(this));
+		HandlerList.unregisterAll(this);
 	}
 	
 	
@@ -90,6 +105,12 @@ public class Match {
 	 * @param player The player who voted
 	 */
 	public void voteToSkip(Player player) {
+		// Check if voting is open
+		if (!votingAllowed) {
+			player.sendMessage("The vote-skip period for this challenge is closed!");
+			return;
+		}
+		// Check if player already voted
 		if (votes.add(player.getUniqueId()))
 			announce(String.format("%s has voted to skip this challenge!", player.getDisplayName()));
 		else
@@ -99,17 +120,31 @@ public class Match {
 			skipChallenge();
 	}
 	
+	@EventHandler
+	public void onChallengeCompleted(ChallengeCompletedEvent event) {
+		Challenge completedChallenge = event.getChallenge();
+		if (completedChallenge != currentChallenge)
+			return;
+		// Increment score
+		Player player = event.getPlayer();
+		UUID playerId = player.getUniqueId();
+		playersAndScores.put(playerId, playersAndScores.get(playerId) + 1);
+		announce(String.format("%s has completed the challenge and earned a point!", player.getDisplayName()));
+		// Disable voting
+		endVotingPeriod();
+	}
+	
 	/**
 	 * Skip the currently running challenge. This will cancel any currently running
-	 * timer, end the current challenge, generate a new challenge, and restart the
-	 * challenge timer.
+	 * timers, end the current challenge, generate a new challenge, and restart the
+	 * challenge and voting periods.
 	 */
 	public void skipChallenge() {
-		cancelTimer();
 		endChallenge();
 		announce("The current challenge was vote-skipped! Your next challenge is:");
 		generateNewChallenge();
-		startChallengeTimer();
+		startChallengePeriod();
+		startVotingPeriod();
 	}
 	
 	/**
@@ -122,51 +157,78 @@ public class Match {
 
 	/**
 	 * Called when the preparation period timer ends. This will generate the first
-	 * task and start the challenge timer.
+	 * task and start the challenge and voting periods.
 	 */
 	private void prepTimerEnded() {
 		announce("The preparation period is over! Your first challenge is:");
 		generateNewChallenge();
-		startChallengeTimer();
+		startChallengePeriod();
+		startVotingPeriod();
 	}
 	
 	/**
 	 * Called when the challenge timer ends. This will end the current challenge,
-	 * generate a new challenge, and restart the challenge timer.
+	 * generate a new challenge, and restart the challenge and voting periods.
 	 */
 	private void challengeTimerEnded() {
 		endChallenge();
 		announce("Times up! Your next challenge is:");
 		generateNewChallenge();
-		startChallengeTimer();
+		startChallengePeriod();
+		startVotingPeriod();
 	}
 	
 	/**
-	 * Start prep timer.
+	 * Start the prep timer.
 	 */
-	private void startPrepTimer() {
-		schedule(this::prepTimerEnded, prepTimerMins);
+	private void startPrepPeriod() {
+		mainTimerTaskId = schedule(this::prepTimerEnded, prepTimerMins);
 	}
 	
 	/**
-	 * Start challenge timer.
+	 * Start the voting timer.
 	 */
-	private void startChallengeTimer() {
-		schedule(this::challengeTimerEnded, challengeTimerMins);
+	private void startVotingPeriod() {
+		votingAllowed = true;
+		votes.clear();
+		votingTimerTaskId = schedule(this::endVotingPeriod, votingTimerMins);
+	}
+	
+	private void endVotingPeriod() {
+		closeVoting();
+		announce("The skip-vote period is now closed!");
 	}
 	
 	/**
-	 * Cancel timer.
+	 * Closes the voting period without announcing anything.
 	 */
-	private void cancelTimer() {
-		Bukkit.getScheduler().cancelTask(timerTaskId);
+	private void closeVoting() {
+		votingAllowed = false;
+		votes.clear();
 	}
 	
 	/**
-	 * End challenge.
+	 * Start the challenge timer.
+	 */
+	private void startChallengePeriod() {
+		mainTimerTaskId = schedule(this::challengeTimerEnded, challengeTimerMins);
+	}
+	
+	/**
+	 * Cancel all running timers.
+	 */
+	private void cancelTimers() {
+		BukkitScheduler bs = Bukkit.getScheduler();
+		bs.cancelTask(mainTimerTaskId);
+		bs.cancelTask(votingTimerTaskId);
+	}
+	
+	/**
+	 * End the current challenge.
 	 */
 	private void endChallenge() {
-		votes.clear();
+		cancelTimers();
+		closeVoting();
 		currentChallenge.end();
 	}
 	
@@ -194,8 +256,8 @@ public class Match {
 	 * @param runnable The task to run
 	 * @param delayMins The number of minutes to wait
 	 */
-	private void schedule(Runnable runnable, long delayMins) {
-		timerTaskId = Bukkit.getScheduler().scheduleSyncDelayedTask(ChallengeRunPlugin.instance, runnable, delayMins * 60 * 20 );
+	private int schedule(Runnable runnable, long delayMins) {
+		return Bukkit.getScheduler().scheduleSyncDelayedTask(ChallengeRunPlugin.instance, runnable, delayMins * 60 * 20 );
 	}
 
 }
